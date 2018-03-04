e3f4025fca11dc3d821b61bbcff45efc8966cde4
nmjmdr <narasimha.gm@gmail.com>
2018-03-05 09:59:34 +1100

    Added more tests to circuit breaker

--- tests/circuit-breaker.spec.js
+++ tests/circuit-breaker.spec.js
@@ -45,9 +45,26 @@ describe('Given circuit-breaker',()=>{
           expect(r.invoked).to.be.equal(secondary);
         });
       })
-    });
 
-    describe('Then invoked, with primary having errors',()=>{
+      describe('When the secondary also fails',()=>{
+        it('Should return an error',()=>{
+          const primary = sandbox.stub().returns(Promise.reject("Error"));
+          const secondary = sandbox.stub().returns(Promise.reject("Error"));
+          const cb = circuitbreaker.create(primary, secondary, (stats)=>{
+            return false
+          });
+
+          return cb([1])
+          .catch((err)=>{
+            expect(primary.called).to.be.true;
+            expect(secondary.called).to.be.true;
+            expect(err).to.exist;
+          });
+
+
+        });
+      });
+
       describe('Then for subsquent invocations',()=>{
         it('Should keep invoking the secondary',()=>{
           const primaryReturn = 100;
@@ -76,9 +93,52 @@ describe('Given circuit-breaker',()=>{
           return Promise.all(promises);
         })
       });
+
+      describe('Should invoke secondary, but later when secondary encounters an error (probe policy does not matter)',()=>{
+        it('Should try and invoke the primary',()=>{
+          const primaryReturn = 100;
+          const secondaryReturn = 200;
+          let calls = 0
+          const primary = ()=>{
+            calls++
+            if(calls > 1) {
+              return Promise.resolve(primaryReturn);
+            } else {
+              return Promise.reject("Error");
+            }
+          }
+          let secondaryCalls = 0;
+          const secondary = ()=>{
+            secondaryCalls++
+            if(secondaryCalls == 1) {
+              return Promise.resolve(secondaryReturn);
+            } else {
+              return Promise.reject("Error");
+            }
+          }
+          const cb = circuitbreaker.create(primary, secondary, (stats)=>{
+            return false
+          });
+
+          let promises = [];
+          promises.push(cb([1])
+          .then((r)=>{
+            expect(r.result).to.be.equal(secondaryReturn);
+            expect(r.invoked).to.be.equal(secondary);
+          }));
+
+          promises.push(cb([1])
+          .then((r)=>{
+            expect(r.result).to.be.equal(primaryReturn);
+            expect(r.invoked).to.be.equal(primary);
+          }));
+
+          return Promise.all(promises);
+        })
+      });
     });
 
-    describe('When set a prove policy to try primary',()=>{
+    describe('When set a probe policy to try primary',()=>{
       describe('Then invoked, with primary having errors',()=>{
         describe('Then for subsquent invocations',()=>{
           it('Should try invoking the primary when the probe policy asks to probe primary',()=>{
@@ -114,37 +174,31 @@ describe('Given circuit-breaker',()=>{
             return Promise.all(promises);
           })
         });
-      });
-    });
 
-    describe('When set a prove policy to try primary',()=>{
-      describe('Then invoked, with primary having errors',()=>{
-        describe('Then for subsquent invocations',()=>{
-          describe('Then it should probe primary and when it still fails',()=>{
-            it('Should invoke the secondary',()=>{
-              const primaryReturn = 100;
-              const secondaryReturn = 200;
-              const primary = sandbox.stub().returns(Promise.reject('Error'));
-              const secondary = sandbox.stub().returns(Promise.resolve(secondaryReturn));
-              const cb = circuitbreaker.create(primary, secondary, (stats)=>{
-                return stats.callsToSecondary > 1;
-              });
+        describe('Then it should probe primary and when it still fails',()=>{
+          it('Should invoke the secondary',()=>{
+            const primaryReturn = 100;
+            const secondaryReturn = 200;
+            const primary = sandbox.stub().returns(Promise.reject('Error'));
+            const secondary = sandbox.stub().returns(Promise.resolve(secondaryReturn));
+            const cb = circuitbreaker.create(primary, secondary, (stats)=>{
+              return stats.callsToSecondary > 1;
+            });
 
-              let promises = [];
-              promises.push(cb([1])
-              .then((r)=>{
-                expect(r.result).to.be.equal(secondaryReturn);
-                expect(r.invoked).to.be.equal(secondary);
-              }));
+            let promises = [];
+            promises.push(cb([1])
+            .then((r)=>{
+              expect(r.result).to.be.equal(secondaryReturn);
+              expect(r.invoked).to.be.equal(secondary);
+            }));
 
-              promises.push(cb([1])
-              .then((r)=>{
-                expect(r.result).to.be.equal(secondaryReturn);
-                expect(r.invoked).to.be.equal(secondary);
-              }));
+            promises.push(cb([1])
+            .then((r)=>{
+              expect(r.result).to.be.equal(secondaryReturn);
+              expect(r.invoked).to.be.equal(secondary);
+            }));
 
-              return Promise.all(promises);
-            })
+            return Promise.all(promises);
           })
         });
       });
