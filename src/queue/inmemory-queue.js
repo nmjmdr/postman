81480b2c4185fce274446373c37cf6d9e0b3bc01
nmjmdr <narasimha.gm@gmail.com>
2018-03-04 16:49:02 +1100

    Added in memory queue

--- src/queue/inmemory-queue.js
+++ src/queue/inmemory-queue.js
@@ -1,8 +1,67 @@
-module.exports = {
-  read: ()=>{
-    return Promise.resolve(true);
-  },
-  delete: ()=>{
-    return Promise.resolve(true);
+let map = {}
+
+function setVisiblityTimer(item, timeout) {
+  setTimeout(()=>{
+    item.assignedTo = null
+  },timeout);
+}
+
+function curry(queue) {
+  function read(assignedTo) {
+    if(queue.buffer.length === 0) {
+      return null;
+    }
+    const item = queue.buffer.find((item)=>{
+      return !item.assignedTo
+    });
+    item.assignedTo = assignedTo;
+    setVisiblityTimer(item, queue.visiblityTimeout);
+    //return Promise.resolve(item);
+    return item;
   }
+
+  function deleteItem(item) {
+    if(queue.buffer.length === 0) {
+      return Promise.resolve(true);
+    }
+    queue.buffer = queue.buffer.filter((item)=>{
+      return item.id == itemToDelete.id
+    });
+    //return Promise.resolve(true);
+    return true;
+  }
+
+  function add(item) {
+    queue.buffer.push(item);
+    //return Promise.resolve(true);
+    return true;
+  }
+
+  return {
+    read: read,
+    delete: deleteItem,
+    add: add
+  }
+}
+
+function create(configuration) {
+  if(!map[configuration.name]) {
+    map[configuration.name] = {
+      visiblityTimeout: configuration.visiblityTimeout,
+      buffer: []
+    };
+  }
+  return curry(map[configuration.name]);
+}
+
+function get(queueName) {
+  if(!map[queueName]) {
+    return null;
+  }
+  return curry(map[queueName]);
+}
+
+module.exports = {
+  create: create,
+  get: get
 }
