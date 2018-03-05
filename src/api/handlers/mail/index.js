52d7742089c6fd0b172dbfd5aa322685c1ba423e
nmjmdr <narasimha.gm@gmail.com>
2018-03-05 13:35:44 +1100

    Added validation of send mail request

--- src/api/handlers/mail/index.js
+++ src/api/handlers/mail/index.js
@@ -1,6 +1,20 @@
+const validator = require('./validator');
+
 
 function sendMail(req, res, next) {
-  
+  const errors = validator.validate(req.body);
+  if(errors && errors.length > 0) {
+    res.send(400, errors);
+    next(false);
+    return;
+  }
+  // start processing,
+  // it should return some unique id with which the errors can be tracked later
+  // now it should b queued for processing
+  res.send(200, {
+    ok: true
+  });
+  next();
 }
 
 module.exports = sendMail;
