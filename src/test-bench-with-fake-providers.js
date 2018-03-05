803c604f4d8e5433ec1112d9c011b4e4420c88e7
nmjmdr <narasimha.gm@gmail.com>
2018-03-06 00:40:11 +1100

    Refactored test bench

---
+++ src/test-bench-with-fake-providers.js
@@ -0,0 +1,16 @@
+
+const queueFactory = require('./queue/factory');
+const postbox = require('./mailer/postbox');
+const fakeProviderFactory = require('./mailer/providers/fake-provider-factory');
+const config = require('../config');
+const singleProcBench = require('./single-process-api-worker');
+
+const configuration = {
+  queue: queueFactory.get(config.queue),
+  postbox: postbox.create(fakeProviderFactory('Fake Provider *Lamda', 2),fakeProviderFactory('Fake Provider *Delta', 2)),
+  ledger:  require('./ledger/no-store-ledger'),
+  schedule: 5000,
+  workerId: 'single-worker'
+}
+
+singleProcBench.setup(configuration);
