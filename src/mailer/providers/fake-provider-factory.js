152cb25b63f27037a3fe3e536be03fc6dc4452ee
nmjmdr <narasimha.gm@gmail.com>
2018-03-06 00:23:29 +1100

    added test api-and-worker

---
+++ src/mailer/providers/fake-provider-factory.js
@@ -0,0 +1,15 @@
+let calls = 0;
+
+module.exports = (name, failAfter) => {
+  return (mail)=>{
+    console.log("------------------------------------------------");
+    calls++
+    if(calls === failAfter) {
+      calls = 0;
+      console.log("%s: failed to send mail - %s",name, mail.id);
+      return Promise.reject("%s: failed to send mail",name);
+    }
+    console.log("%s: sent mail - %s",name, mail.id);
+    return Promise.resolve(true);
+  }
+}
