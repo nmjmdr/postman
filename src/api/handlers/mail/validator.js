5f9b2ce0b87aa0ff6e572edff3bc41fceb7fa962
nmjmdr <narasimha.gm@gmail.com>
2018-03-05 13:35:44 +1100

    Added validation of send mail request

--- src/api/handlers/mail/validator.js
+++ src/api/handlers/mail/validator.js
@@ -1,7 +1,30 @@
-function validate(body) {
+const s = require('strummer');
 
+const MaxBatchSize = 10;
+const MaxEmailsInTo = 100;
+
+const email = new s.object({
+  email: new s.email()
+});
+
+const content = new s.object({
+  type: new s.enum({values: ['text/plain']}),
+  value: s.string()
+})
+
+const mailMessage = new s.object({
+  to: s.array({min: 1, max: MaxEmailsInTo, of: email}),
+  from: email,
+  subject: s.string(),
+  content: s.array({of: content})
+});
+
+const schema = new s.array({min: 1, max: MaxBatchSize, of: mailMessage});
+
+function validate(body) {
+  return schema.match(body);
 }
 
 module.exports = {
-
+  validate: validate
 }
--- src/api/routes.js
+++ src/api/routes.js
@@ -1,7 +1,12 @@
 const status = require('./handlers/status');
+const mail = require('./handlers/mail');
 
 exports.create = (app) => {
   app.get({ name: "status",
     path: "/status"
   }, [status]);
+
+  app.post({ name: "send-mail",
+    path: "/mail"
+  }, [mail]);
 }
