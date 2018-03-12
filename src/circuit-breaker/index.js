87920d9a4a8fa1b8087c76893572b9cdde0f9903
nmjmdr <narasimha.gm@gmail.com>
2018-03-12 23:44:40 +1100

    refactored cb

--- src/circuit-breaker/index.js
+++ src/circuit-breaker/index.js
@@ -1,59 +1,57 @@
-function safeInvoke(fn1, fn2, args, isUnavailableError) {
-  return invokeFunction(fn1, args)
+function safeInvoke(fn1, fn2, args, onErrorTryOther) {
+  return wrappedInvoke(fn1,args)
   .catch((err)=>{
-    if(isUnavailableError(err, fn1)) {
-      return invokeFunction(fn2, args)
+    if(onErrorTryOther(err, fn1)) {
+      return wrappedInvoke(fn2, args);
     } else {
       throw err;
-    }
-  });
-}
-
-function invokeFunction(fn, args) {
-  return fn(...args)
-  .then((result)=>{
-    return {
-      invoked: fn,
-      result: result
     }
   })
 }
 
-function shouldFlip(usedFn, fn, alternate) {
-  return (usedFn === fn)? fn : alternate;
+function wrappedInvoke(fn, args) {
+  return fn(...args)
+  .then((r)=>{
+    return {
+      invoked: fn,
+      result: r
+    }
+  })
 }
 
-function create(primary, secondary, probePolicy, isUnavailableError) {
-  let fn = primary;
-  let stats = {
-    callsToSecondary: 0
-  };
 
-  function computeStats(usedFn) {
-    if(usedFn === secondary) {
+function create(primary, secondary, shouldProbe, onErrorTryOther) {
+  let fn1 = primary;
+  let fn2 = secondary;
+  let stats = {
+    callsToSecondary: 0,
+    ticksSincePrimaryFailed: null,
+  }
+
+  function setStats(invocation) {
+    if(invocation.invoked === secondary) {
       stats.callsToSecondary++;
+      stats.ticksSincePrimaryFailed = stats.ticksSincePrimaryFailed? stats.ticksSincePrimaryFailed : new Date().getTime()
     } else {
       stats.callsToSecondary = 0;
+      stats.ticksSincePrimaryFailed = null;
     }
   }
 
-
-  function invoke(args) {
-    let alternate = (fn === primary)? secondary : primary;
-    if(fn === secondary && probePolicy(stats)) {
-      fn = primary;
-      alternate = secondary;
+  function invoke(args){
+    if(fn1 === secondary && shouldProbe(stats)) {
+      fn1 = primary;
+      fn2 = secondary;
     }
-
-    return safeInvoke(fn, alternate, args, isUnavailableError)
-    .then((result)=>{
-      computeStats(result.invoked, fn);
-      fn = shouldFlip(result.invoked, fn, alternate);
-      return result;
+    return safeInvoke(fn1, fn2, args, onErrorTryOther)
+    .then((invocation)=>{
+      setStats(invocation)
+      fn1 = (invocation.invoked === primary)? primary : secondary;
+      fn2 = (invocation.invoked === secondary)? primary : secondary;
+      return invocation;
     })
   }
-
-  return invoke
+  return invoke;
 }
 
 module.exports = {
